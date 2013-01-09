69cf8317cf882cc382e649ae0c640d3837c6d7b1
berrybana <hana@mocoplex.com>
2013-01-09 19:12:15 +0900

    mod SubAdlibAdViewAdmob.java

--- adlibrTestProject/src/test/adlib/project/ads/SubAdlibAdViewAdmob.java
+++ adlibrTestProject/src/test/adlib/project/ads/SubAdlibAdViewAdmob.java
@@ -24,69 +24,68 @@ public class SubAdlibAdViewAdmob extends SubAdlibAdViewCore  {
 	
 	protected com.google.ads.AdView ad;
 	protected boolean bGotAd = false;
-
+    
 	public SubAdlibAdViewAdmob(Context context) {
 		this(context,null);
-	}	
+	}
 	
 	public SubAdlibAdViewAdmob(Context context, AttributeSet attrs) {
 		
 		super(context, attrs);
-
+        
 		// 여기에 ADMOB ID 를 입력하세요.
 		String admobID = "ADMOB ID";
-
+        
 		ad = new com.google.ads.AdView((Activity) this.getContext(), com.google.ads.AdSize.BANNER, admobID);
-
-		// 광고 뷰의 위치 속성을 제어할 수 있습니다. 
+        
+		// 광고 뷰의 위치 속성을 제어할 수 있습니다.
 		this.setGravity(Gravity.CENTER);
 		
 		ad.setAdListener( new com.google.ads.AdListener() {
-
-			@Override
+            
+			
 			public void onDismissScreen(Ad arg0) {
-
+                
 				
 			}
-
-			@Override
+            
 			public void onFailedToReceiveAd(Ad arg0, ErrorCode arg1) {
 				if(!bGotAd)
-					failed();				
+					failed();
 			}
-
-			@Override
+            
 			public void onLeaveApplication(Ad arg0) {
-
+                
 				
 			}
-
-			@Override
+            
 			public void onPresentScreen(Ad arg0) {
-
+                
 				
 			}
-
-			@Override
+            
 			public void onReceiveAd(Ad arg0) {
 				
+				if(bGotAd)
+					return;
+				
 				bGotAd = true;
+				
 				// 광고를 받아왔으면 이를 알려 화면에 표시합니다.
-				gotAd();				
+				gotAd();
 			}
-
+            
 		});
-
-		this.addView(ad);		
+        
+		this.addView(ad);
 	}
-
+    
 	// 스케줄러에의해 자동으로 호출됩니다.
-	// 실제로 광고를 보여주기 위하여 요청합니다.	
+	// 실제로 광고를 보여주기 위하여 요청합니다.
 	public void query()
-	{		
+	{
+		bGotAd = false;
 		ad.loadAd(request);
-		if(bGotAd)
-			gotAd();
 	}
 	private com.google.ads.AdRequest request = new com.google.ads.AdRequest();
 	
@@ -95,7 +94,7 @@ public class SubAdlibAdViewAdmob extends SubAdlibAdViewCore  {
 		if(ad != null)
 		{
 			ad.destroy();
-			ad = null;			
+			ad = null;
 		}
 		
 		super.onDestroy();
@@ -105,7 +104,7 @@ public class SubAdlibAdViewAdmob extends SubAdlibAdViewCore  {
 	{
 		if(ad != null)
 		{
-			ad.stopLoading();						
+			ad.stopLoading();
 		}
 		
 		super.clearAdView();
