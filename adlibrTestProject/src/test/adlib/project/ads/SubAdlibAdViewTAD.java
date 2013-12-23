f26aae6b070a37f214c26b333572996fb3f655dd
berrybana <hana@mocoplex.com>
2013-12-23 12:05:58 +0900

    update tad sdk and subview

--- adlibrTestProject/src/test/adlib/project/ads/SubAdlibAdViewTAD.java
+++ adlibrTestProject/src/test/adlib/project/ads/SubAdlibAdViewTAD.java
@@ -6,7 +6,7 @@
  */
 
 /*
- * confirmed compatible with T ad SDK 3.1.1.6
+ * confirmed compatible with T ad SDK 3.3.3.6
  */
 
 package test.adlib.project.ads;
@@ -44,7 +44,7 @@ public class SubAdlibAdViewTAD extends SubAdlibAdViewCore  {
     {
 		ad = new com.skplanet.tad.AdView(this.getContext());
 		ad.setClientId(tAdId);
-		ad.setSlotNo(Slot.RICHMEDIA_320X50_INLINE);
+		ad.setSlotNo(Slot.BANNER);
 		
 		/*  새로운 받은 광고가 Display 되는 Animation 효과를 설정합니다.
 		 * NONE							효과없음
@@ -61,8 +61,8 @@ public class SubAdlibAdViewTAD extends SubAdlibAdViewCore  {
 		 * ROTATE3D_180_VERTICAL		세로로 3D 회전 효과
 		 */
 		ad.setAnimationType(AnimationType.NONE);
-		// 새로운 광고를 요청하는 주기를 입력합니다. 최소값은 15, 최대값은 60 입니다.
-		ad.setRefreshInterval(20);
+		// 새로운 광고를 요청하는 주기를 입력합니다. 최소값은 15, 최대값은 60 입니다. 0으로 설정한 경우 1번의 광고만 수신합니다.(mediation 사용하는 경우엔 0으로 설정)
+		ad.setRefreshInterval(0);
 		/* 광고 View 의 Background 의 사용 유무를 설정합니다.
 		 * useBackFill 속성을 true 로 설정하면 각 광고마다 광고주가 설정한 배경색이 그려지고 false 인 경우 투명(0x00000000)으로 나타납니다. */
 		ad.setUseBackFill(true);
@@ -126,6 +126,21 @@ public class SubAdlibAdViewTAD extends SubAdlibAdViewCore  {
 				
 			}
 			
+			@Override
+			public void onAdDismissScreen() {
+				
+			}
+			
+			@Override
+			public void onAdLeaveApplication() {
+				
+			}
+			
+			@Override
+			public void onAdPresentScreen() {
+				
+			}
+			
 		});
 		
 		// 광고 뷰의 위치 속성을 제어할 수 있습니다.
@@ -143,7 +158,7 @@ public class SubAdlibAdViewTAD extends SubAdlibAdViewCore  {
         
         queryAd();
         
-		ad.startAd();
+		ad.loadAd();
 		
 		// 3초 이상 리스너 응답이 없으면 다음 플랫폼으로 넘어갑니다.
 		Handler adHandler = new Handler();
@@ -183,11 +198,21 @@ public class SubAdlibAdViewTAD extends SubAdlibAdViewCore  {
 	
 	public void onResume()
 	{
+		if(ad != null)
+		{
+			ad.loadAd();
+		}
+		
 		super.onResume();
 	}
 	
 	public void onPause()
 	{
+		if(ad != null)
+		{
+			ad.stopAd();
+		}
+		
 		super.onPause();
 	}
 	
