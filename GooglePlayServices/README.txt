174303b9219a9a8ed2dc290f5308be6ea3e963eb
berrybana <hana@mocoplex.com>
2014-02-25 20:04:03 +0900

    update for google-play-services

---
+++ GooglePlayServices/README.txt
@@ -0,0 +1,15 @@
+<<Google Play Service library를 이용하여 앱을 만드는 경우>>
+
+admob, admob 네트워크를 이용하실 때,
+admob SDK는 삭제하시고, admob SubAdView 는 현재 이 폴더의 파일로 교체 하십시오.
+
+AndroidManifest.xml에 
+
+<meta-data android:name="com.google.android.gms.version"
+	android:value="@integer/google_play_services_version"/>
+<activity android:name="com.google.android.gms.ads.AdActivity"
+	android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize" />
+	
+위의 내용을 추가하십시오.
+
+애드립 설정이나, admob 외의 타 플랫폼에 대한 부분은 동일합니다.
---
+++ GooglePlayServices/SubAdlibAdViewAdmob.java
@@ -0,0 +1,204 @@
+/*
+ * adlibr - Library for mobile AD mediation.
+ * http://adlibr.com
+ * Copyright (c) 2012-2013 Mocoplex, Inc.  All rights reserved.
+ * Licensed under the BSD open source license.
+ */
+
+
+package test.adlib.project.ads;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.mocoplex.adlib.AdlibManager;
+import com.mocoplex.adlib.SubAdlibAdViewCore;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.Gravity;
+
+/*
+AndroidManifest.xml 에 아래 내용을 추가해주세요.
+
+ <meta-data android:name="com.google.android.gms.version"
+	android:value="@integer/google_play_services_version"/>
+ <activity android:name="com.google.android.gms.ads.AdActivity"
+	android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize"/>
+*/
+
+public class SubAdlibAdViewAdmob extends SubAdlibAdViewCore  {
+	
+	protected AdView ad;
+	protected boolean bGotAd = false;
+	
+	// 여기에 ADMOB ID 를 입력하세요.
+	static String admobID = "ADMOB_ID";
+    
+	public SubAdlibAdViewAdmob(Context context) {
+		this(context,null);
+	}
+	
+	public SubAdlibAdViewAdmob(Context context, AttributeSet attrs) {
+		
+		super(context, attrs);
+		
+		initAdmobView();
+	}
+	
+	public void initAdmobView()
+	{
+		ad = new AdView((Activity)getContext());
+		ad.setAdUnitId(admobID);
+		ad.setAdSize(AdSize.SMART_BANNER);
+		
+		// 광고 뷰의 위치 속성을 제어할 수 있습니다.
+		this.setGravity(Gravity.CENTER);
+		
+		ad.setAdListener( new AdListener() {
+
+			@Override
+			public void onAdFailedToLoad(int errorCode) {
+				
+				bGotAd = true;
+				failed();
+			}
+
+			@Override
+			public void onAdLoaded() {
+				
+				bGotAd = true;
+				queryAd();
+				// 광고를 받아왔으면 이를 알려 화면에 표시합니다.
+				gotAd();
+			}
+			
+		});
+	}
+	
+	private AdRequest request = new AdRequest.Builder().build();
+    
+	// 스케줄러에의해 자동으로 호출됩니다.
+	// 실제로 광고를 보여주기 위하여 요청합니다.
+	public void query()
+	{
+		if(ad == null)
+			initAdmobView();
+		
+        this.removeAllViews();
+		this.addView(ad);
+		
+		ad.loadAd(request);
+        
+        // 5초 이상 리스너 응답이 없으면 다음 플랫폼으로 넘어갑니다.
+		Handler adHandler = new Handler();
+		adHandler.postDelayed(new Runnable() {
+            
+			@Override
+			public void run() {
+				if(bGotAd)
+					return;
+				else
+				{
+					failed();
+					if(ad != null)
+					{
+                    	SubAdlibAdViewAdmob.this.removeView(ad);
+                    	ad.destroy();
+                    	ad = null;
+                    }
+                    bGotAd = false;
+				}
+			}
+            
+		}, 5000);
+	}
+	
+	public void onDestroy()
+	{
+		if(ad != null)
+		{
+			this.removeView(ad);
+			ad.destroy();
+			ad = null;
+		}
+		
+		super.onDestroy();
+	}
+	
+	public void clearAdView()
+	{
+		if(ad != null)
+		{
+        	this.removeView(ad);
+		}
+		
+        super.clearAdView();
+	}
+	
+	public void onResume()
+	{
+        super.onResume();
+	}
+	
+	public void onPause()
+	{
+        super.onPause();
+	}
+	
+	public static void loadInterstitial(Context ctx, final Handler h)
+	{
+		// Create the interstitial
+		final InterstitialAd interstitial = new InterstitialAd((Activity)ctx);
+		interstitial.setAdUnitId(admobID);
+
+	    // Create ad request
+	    AdRequest adRequest = new AdRequest.Builder().build();
+
+	    // Begin loading your interstitial
+	    interstitial.loadAd(adRequest);
+
+	    // Set Ad Listener to use the callbacks below
+	    interstitial.setAdListener(new AdListener() {
+	    	
+
+			@Override
+			public void onAdClosed() {
+				
+				if(h != null)
+	 			{
+	 				h.sendMessage(Message.obtain(h, AdlibManager.INTERSTITIAL_CLOSED, "ADMOB"));
+	 			}
+			}
+
+			@Override
+			public void onAdFailedToLoad(int errorCode) {
+				
+				if(h != null)
+	 			{
+	 				h.sendMessage(Message.obtain(h, AdlibManager.DID_ERROR, "ADMOB"));
+	 			}
+			}
+
+			@Override
+			public void onAdLoaded() {
+				
+				if(interstitial.isLoaded())
+				{
+					if(h != null)
+		 			{
+		 				h.sendMessage(Message.obtain(h, AdlibManager.DID_SUCCEED, "ADMOB"));
+		 			}
+					
+					interstitial.show();
+				}
+			}
+	    	
+	    });
+	}
+}
