1a7dcc86eed083318d297def220cb83e061c794d
berrybana <hana@mocoplex.com>
2014-03-20 14:23:38 +0900

    update unity

---
+++ AdlibUnityPlugin_Android_1.2.1/Assets/Plugins/AdlibPlugin/AdlibPlugin.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System;
+
+public class AdlibPlugin : MonoBehaviour {
+
+	public class BannerSize
+	{
+		private string bannerSize;
+		private BannerSize(string value)
+		{
+			this.bannerSize = value;
+		}
+		
+		public override string ToString()
+		{
+			return bannerSize;
+		}
+
+		public static BannerSize Banner = new BannerSize("BANNER");
+		public static BannerSize Banner_320x50 = new BannerSize("BANNER_320X50");
+	}
+
+	public class BannerAlign
+	{
+		private string bannerAlign;
+		private BannerAlign(string value)
+		{
+			this.bannerAlign = value;
+		}
+		
+		public override string ToString()
+		{
+			return bannerAlign;
+		}
+		
+		public static BannerAlign Left = new BannerAlign("LEFT");
+		public static BannerAlign Right = new BannerAlign("RIGHT");
+		public static BannerAlign Center = new BannerAlign("CENTER");
+	}
+
+	public class PopButton
+	{
+		private string buttonColor;
+		private PopButton(string value)
+		{
+			this.buttonColor = value;
+		}
+		
+		public override string ToString()
+		{
+			return buttonColor;
+		}
+		
+		public static PopButton White = new PopButton("WHITE");
+		public static PopButton Black = new PopButton("BLACK");
+	}
+
+	public class PopAlign
+	{
+		private string align;
+		private PopAlign(string value)
+		{
+			this.align = value;
+		}
+		
+		public override string ToString()
+		{
+			return align;
+		}
+		
+		public static PopAlign Top = new PopAlign("TOP");
+		public static PopAlign Bottom = new PopAlign("BOTTOM");
+		public static PopAlign Left = new PopAlign("LEFT");
+		public static PopAlign Right = new PopAlign("RIGHT");
+	}
+
+	public class RewardLinkAlign
+	{
+		private string align;
+		private RewardLinkAlign(string value)
+		{
+			this.align = value;
+		}
+		
+		public override string ToString()
+		{
+			return align;
+		}
+		
+		public static RewardLinkAlign LeftTop = new RewardLinkAlign("LEFT_TOP");
+		public static RewardLinkAlign RightTop = new RewardLinkAlign("RIGHT_TOP");
+		public static RewardLinkAlign LeftBottom = new RewardLinkAlign("LEFT_BOTTOM");
+		public static RewardLinkAlign RightBottom = new RewardLinkAlign("RIGHT_BOTTOM");
+	}
+	
+	static AndroidJavaClass adlibPluginClass;
+	static AndroidJavaClass unityPlayer;
+	static AndroidJavaObject currActivity;
+
+	public static event Action<string> ReceivedInterstitial = delegate {};
+	public static event Action FailedToReceiveInterstitial = delegate {};
+	public static event Action DismissedInterstitial = delegate {};
+	public static event Action ReceivedPopBanner = delegate {};
+	public static event Action FailedToReceivePopBanner = delegate {};
+	public static event Action DismissedPopBanner = delegate {};
+
+	void Awake() {
+		gameObject.name = this.GetType().ToString();
+		SetCallbackHandlerName(gameObject.name);
+		DontDestroyOnLoad(this);
+	}
+	
+	public static void InitializeAdlib(string ads) {
+		if(adlibPluginClass == null)
+			adlibPluginClass = new AndroidJavaClass("com.mocoplex.adlib.unity.AdlibPlugin");
+		unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		currActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		adlibPluginClass.CallStatic("Initialize", currActivity, ads);
+	}
+
+	public static void SetCallbackHandlerName(string name) {
+		if(adlibPluginClass == null)
+			adlibPluginClass = new AndroidJavaClass("com.mocoplex.adlib.unity.AdlibPlugin");
+		adlibPluginClass.CallStatic("setCallbackHandlerName", name);
+	}
+
+	public static void ShowBanner(BannerSize size, bool useHouseBanner, bool positionAtTop) {
+		adlibPluginClass.CallStatic("ShowBanner", size.ToString(), useHouseBanner, positionAtTop, 0);
+	}
+	
+	public static void ShowBanner(BannerSize size, bool useHouseBanner, bool positionAtTop, int padding) {
+		adlibPluginClass.CallStatic("ShowBanner", size.ToString(), useHouseBanner, positionAtTop, padding);
+	}
+
+	public static void ShowBanner(BannerSize size, bool useHouseBanner, bool positionAtTop, BannerAlign align) {
+		adlibPluginClass.CallStatic("ShowBanner", size.ToString(), useHouseBanner, positionAtTop, 0, align.ToString());
+	}
+	
+	public static void ShowBanner(BannerSize size, bool useHouseBanner, bool positionAtTop, int padding, BannerAlign align) {
+		adlibPluginClass.CallStatic("ShowBanner", size.ToString(), useHouseBanner, positionAtTop, padding, align.ToString());
+	}
+	
+	public static void HideBanner() {
+		adlibPluginClass.CallStatic("HideBanner");
+	}
+
+	public static void LoadInterstitialAd() {
+		adlibPluginClass.CallStatic("LoadInterstitialAd");
+	}
+
+	public static void ShowPopBanner(string frameColor, PopButton btnColor, bool useInAnim, bool useOutAnim, PopAlign btnAlign, int padding) {
+		adlibPluginClass.CallStatic("ShowPopBanner", frameColor, btnColor.ToString(), useInAnim, useOutAnim, btnAlign.ToString(), padding);
+	}
+	
+	public static void HidePopBanner() {
+		adlibPluginClass.CallStatic("HidePopBanner");
+	}
+
+	public static void ShowRewardLink(string linkId, RewardLinkAlign linkAlign, int x, int y) {
+		adlibPluginClass.CallStatic("ShowRewardLink", linkId, linkAlign.ToString(), x, y);
+	}
+
+	public static void HideRewardLink() {
+		adlibPluginClass.CallStatic("HideRewardLink");
+	}
+
+	public void OnReceivedInterstitialAd(string platform)
+	{
+		ReceivedInterstitial(platform);
+	}
+	
+	public void OnFailedToReceiveInterstitialAd(string unusedMessage)
+	{
+		FailedToReceiveInterstitial();
+	}
+	
+	public void OnClosedInterstitialAd(string unusedMessage)
+	{
+		DismissedInterstitial();
+	}
+
+	public void OnReceivedPopBanner(string unusedMessage)
+	{
+		ReceivedPopBanner();
+	}
+	
+	public void OnFailedToReceivePopBanner(string unusedMessage)
+	{
+		FailedToReceivePopBanner();
+	}
+	
+	public void OnClosedPopBanner(string unusedMessage)
+	{
+		DismissedPopBanner();
+	}
+	
+	public static void StopAds() {
+		adlibPluginClass.CallStatic("Pause");
+	}
+	
+	public static void RestartAds() {
+		adlibPluginClass.CallStatic("Resume");
+	}
+}
---
+++ AdlibUnityPlugin_Android_1.2.1/Assets/Plugins/AdlibPlugin/AdlibTestScript.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Net.Json;
+
+// Example script showing how you can easily call into the AdlibPlugin.
+public class AdlibTestScript : MonoBehaviour {
+
+	Rect rect = new Rect();
+	void OnGUI()
+	{
+		rect.x = 20;
+		rect.y = 300;
+		
+		rect.width = Screen.width * 0.3f;
+		rect.height = Screen.height * 0.1f * 2 / 3;
+		// Make the Enable Button
+		if (GUI.Button(rect, "Hide")) {
+			AdlibPlugin.HideBanner();
+		}
+		
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "Show")) {
+			AdlibPlugin.ShowBanner(AdlibPlugin.BannerSize.Banner, false, false);
+		}
+
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "LoadInterstitial")) {
+			AdlibPlugin.LoadInterstitialAd();
+		}
+
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "ShowPopBanner")) {
+			AdlibPlugin.ShowPopBanner("#ff444444",
+			                          AdlibPlugin.PopButton.White,
+			                          true, 
+			                          false,
+			                          AdlibPlugin.PopAlign.Bottom,
+			                          60);
+		}
+
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "ShowIcon1")) {
+			// RewardLink ID for Test - insert your real ID
+			AdlibPlugin.ShowRewardLink("519c29ffe4b00e029838e9ed",
+			                           AdlibPlugin.RewardLinkAlign.RightTop,
+			                           50,
+			                           60);
+		}
+
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "ShowIcon2")) {
+			// RewardLink ID for Test - insert you real ID
+			AdlibPlugin.ShowRewardLink("519c2a0ee4b00e029838e9ee",
+			                           AdlibPlugin.RewardLinkAlign.LeftTop,
+			                           50,
+			                           60);
+		}
+
+		rect.y = rect.y + rect.height + 10;
+		if (GUI.Button(rect, "HideIcon")) {           
+			AdlibPlugin.HideRewardLink();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		AdlibPlugin.ReceivedInterstitial += HandleReceivedInterstitial;
+		AdlibPlugin.FailedToReceiveInterstitial += HandleFailedToReceiveInterstitial;
+		AdlibPlugin.DismissedInterstitial += HandleDismissedInterstitial;
+		AdlibPlugin.ReceivedPopBanner += HandleReceivedPopBanner;
+		AdlibPlugin.FailedToReceivePopBanner += HandleFailedToReceivePopBanner;
+		AdlibPlugin.DismissedPopBanner += HandleDismissedPopBanner;
+
+		JsonObjectCollection ads = new JsonObjectCollection();
+		ads.Add(new JsonStringValue("ADLIB","52f30fc5e4b0c16dd4c0f898"));  // ADLIB_API_KEY for Test - insert your real KEY
+		ads.Add(new JsonStringValue("ADAM","insert_your_adam_id"));
+		ads.Add(new JsonStringValue("ADAM_INTERSTITIAL","insert_your_adam_interstitial_id"));
+		ads.Add(new JsonStringValue("ADMOB","insert_your_admob_id"));
+		ads.Add(new JsonStringValue("ADMOB_INTERSTITIAL","insert_your_admob_interstitial_id"));
+		ads.Add(new JsonStringValue("CAULY","insert_your_cauly_id"));
+		ads.Add(new JsonStringValue("CAULY_INTERSTITIAL","insert_your_cauly_interstitial_id"));
+		ads.Add(new JsonStringValue("ADPOST","insert_your_naverAdPost_id"));
+		ads.Add(new JsonStringValue("INMOBI","insert_your_inmobi_id"));
+		ads.Add(new JsonStringValue("INMOBI_INTERSTITIAL","insert_your_inmobi_interstitial_id"));
+		ads.Add(new JsonStringValue("ADMOB_MEDIATION","insert_your_admob_mediation_id"));
+		ads.Add(new JsonStringValue("UPLUS","insert_your_uplus_id"));
+		ads.Add(new JsonStringValue("UPLUS_INTERSTITIAL","insert_your_uplus_interstitial_id"));
+		ads.Add(new JsonStringValue("TAD","insert_your_tad_id"));
+		ads.Add(new JsonStringValue("TAD_INTERSTITIAL","insert_your_tad_interstitial_id"));
+		ads.Add(new JsonStringValue("ADHUB","insert_your_adhub_id"));
+		ads.Add(new JsonStringValue("ADHUB_INTERSTITIAL","insert_your_adhub_interstitial_id"));
+		ads.Add(new JsonStringValue("AMAZON","insert_your_amazon_id"));
+		ads.Add(new JsonStringValue("MEZZO","insert_your_mezzoMan_id"));
+		ads.Add(new JsonStringValue("MILLENNIAL_MEDIA","insert_your_millennialMedia_id"));
+
+		AdlibPlugin.InitializeAdlib(ads.ToString());
+		
+		AdlibPlugin.ShowBanner(AdlibPlugin.BannerSize.Banner_320x50, true, true, 0);
+	}
+
+	void OnApplicationPause(bool pause) {
+		if(pause) {
+			AdlibPlugin.StopAds();
+		}
+		else {
+			AdlibPlugin.RestartAds();
+		}
+	}
+
+	public void HandleReceivedInterstitial(string platform) {
+		print("Received Interstitial Ad : ");
+		print(platform);
+	}
+
+	public void HandleFailedToReceiveInterstitial() {
+		print("Failed Interstitial Ad");
+	}
+
+	public void HandleDismissedInterstitial() {
+		print("Closed Interstitial Ad");
+	}
+
+	public void HandleReceivedPopBanner() {
+		print("Received PopBanner");
+	}
+	
+	public void HandleFailedToReceivePopBanner() {
+		print("Failed PopBanner");
+	}
+	
+	public void HandleDismissedPopBanner() {
+		print("Closed PopBanner");
+	}
+}
